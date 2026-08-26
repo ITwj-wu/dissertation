e4c2b8d8479e42ceb71a1ed9b2bcadffffd9faf4
ITwj-wu <wuwenjing115@126.com>
2026-08-26 22:07:54 +0100

    feat: [project_vue] finished About page and install sass

--- project_vue/src/main.js
+++ project_vue/src/main.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./assets/main.css";
+import "./assets/style.scss"
 
 import App from './App.vue'
 import router from './router'
--- project_vue/src/views/AboutView.vue
+++ project_vue/src/views/AboutView.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About me</h1>
+    <p>I have quite a lot of hobbies, though I’m not an expert at any of them. This blog is where I document all my interests and track my progress on each hobby as I go. No fancy mastery, just honest notes of exploration and small updates along the way.</p>
+    <div class="flex justify-center">
+      <img src="../assets/imgs//happy.png" alt="">
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style lang="scss">
+.about {
+  h1 {
+    font-family: "Dancing Script", cursive;
+    font-size: 42px;
+    color: #E8A0B5;
+    margin-bottom: 30px;
+  }
+  p {
+    font-family: "Dancing Script", cursive;
+    font-size: 20px;
+    color: #E8A0B5;
+    margin-bottom: 30px;
+  }
+  img {
+    width: 500px;
+
   }
 }
+
 </style>
